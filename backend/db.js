dce8d31ece9e93ed6d65d69ace59055ddb8c7990
Jeremiah Madhol Ater <72972656+Ater99@users.noreply.github.com>
2024-04-14 08:12:58 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,16 @@
-// db.js
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Empower360', 'postgres', 'Atermadhol99@', {
- host: 'localhost',
- dialect: 'postgres',
- port: 5432,
-});
+// Creating a new Sequelize instance using the connection string
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 // Test the connection
 sequelize.authenticate()
- .then(() => console.log('Connection has been established successfully.'))
- .catch(error => {
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(error => {
     console.error('Unable to connect to the database:', error);
     process.exit(1); // Exit the process if the connection fails
- });
+  });
 
 module.exports = sequelize;
