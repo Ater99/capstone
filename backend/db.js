497ad35ab1a56295a66395d4f26ed19e4caec7f3
Jeremiah Madhol Ater <72972656+Ater99@users.noreply.github.com>
2024-04-14 08:21:13 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,23 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-// Creating a new Sequelize instance using the connection string
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+// Print out the DATABASE_URL to debug
+console.log('Database URL:', process.env.DATABASE_URL);
 
-// Test the connection
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Note: setting this for demo, adjust for production as needed
+    }
+  }
+});
+
 sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch(error => {
     console.error('Unable to connect to the database:', error);
-    process.exit(1); // Exit the process if the connection fails
+    process.exit(1);
   });
 
 module.exports = sequelize;
